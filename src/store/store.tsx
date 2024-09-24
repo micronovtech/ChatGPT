5f845d6c89661ca9fe61147bc20aa04a9692e506
micronovtech <135284968+micronovtech@users.noreply.github.com>
2024-09-24 22:18:31 +0700

    Update store.tsx

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -7,12 +7,8 @@ import moment from "moment";
 import { ImageSize } from "../services/chatService";
 
 const modalsList = [
-  "gpt-3.5-turbo",
-  "gpt-3.5-turbo-1106",
-  "gpt-3.5-turbo-16k-0613",
-  "gpt-3.5-turbo-16k",
-  "gpt-3.5-turbo-0613",
   "gpt-4",
+  "gpt-4o-mini",
   "gpt-4-0613",
   "gpt-4-0314",
   "gpt-4-1106-preview",
